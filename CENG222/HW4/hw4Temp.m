6af44324e9844463715abb3fd4f09b43efe9d822
alperenovak <alperen.ovak@metu.edu.tr>
2024-05-28 23:07:41 +0300

    Q2 done

--- CENG222/HW4/hw4Temp.m
+++ CENG222/HW4/hw4Temp.m
@@ -1,36 +1,56 @@
-N = 4148;
-lambdaA = 60;  % number of automobiles
-lambdaT = 12;  % number of trucks
+  N = 4148;
+  lambdaA = 60;  % number of automobiles
+  lambdaT = 8;  % number of trucks
 
-TotalWeight = zeros(1, N); % a vector that keeps the total weight for each Monte Carlo run
-for k = 1:N;
-  % first generate the number of passed vehicles for each type from Poisson
-  numA = poissrnd(lambdaA);
-  numT = poissrnd(lambdaT);
-
-  % number of automobiles
-  U = rand;
-
-  % number of trucks
-  U = rand;
-
-	weight = 0; % total weight of vehicles for this run
-
-  % calculate the total weight of automobiles
-  weightA = sum(gamrnd(120, 10, 1, numA));
-
-  % calculate the total weight of trucks
-  weightT = sum(gamrnd(14, 1000, 1, numT));
-
-  TotalWeight(k) = weightA + weightT;
+  TotalWeight = zeros(1, N); % vector for Monte Carlo
+  for k = 1:N;
+    % generate # of vehicles for Poisson
+    numA = 0;
+    numT = 0;
 
 
-end;
+    % number of automobiles
+    F = exp(- lambdaA);
+    U = rand;
+    while(U>=F)
+      numA = numA +1;
+      F = F + exp(-lambdaA) * lambdaA^numA/gamma(numA+1);
+    end;
 
-p_est = sum(TotalWeight > 250000) / N;
-expectedWeight = mean(TotalWeight);
-stdWeight = std(TotalWeight);
+    % number of trucks
+    F = exp(- lambdaT);
+    U = rand;
+    while(U>=F)
+      numT = numT +1;
+      F = F + exp(-lambdaT)*lambdaT^numT/gamma(numT+1);
+    end;
 
-fprintf('Estimated probability = %f\n',p_est);
-fprintf('Expected weight = %f\n',expectedWeight);
-fprintf('Standard deviation = %f\n',stdWeight);
+    weight = 0; % total weight of vehicles for this run
+
+    % calculate the total weight of automobiles
+    % weightA = sum(gamrnd(120, 10, 1, numA));
+
+    weightA = 0;
+    for i = 1:numA
+      weightA = weightA + (-(log(rand(120,1)) * 10));
+    end
+
+    % calculate the total weight of trucks
+    % weightT = sum(gamrnd(14, 1000, 1, numT));
+
+    weightT = 0;
+    for i = 1:numT
+      weightT = weightT + (-(log(rand(14,1)) * 1000));
+    end
+
+    TotalWeight(k) = sum(weightA) + sum(weightT);
+
+  end;
+
+  p_est = mean(TotalWeight > 250000);
+  expectedWeight = mean(TotalWeight);
+  stdWeight = std(TotalWeight);
+
+  fprintf('Estimated probability = %f\n',p_est);
+  fprintf('Expected weight = %f\n',expectedWeight);
+  fprintf('Standard deviation = %f\n',stdWeight);
