3e35ead2521427565276a7141a1ae23e8ae286e7
Alperen Ovak <alperen.ovak@metu.edu.tr>
2025-04-08 20:11:47 +0300

    CENG334 HW1: take the input

--- CENG334/HW1/server.cpp
+++ CENG334/HW1/server.cpp
@@ -1,86 +1,69 @@
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <string.h>
+// server.cpp
+#include <iostream>
+#include <vector>
+#include <string>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <poll.h>
+#include <cstring>
+#include "game_structs.h"
+#include "print_output.h"
+#include <sstream> 
 
-// Structure to hold player configuration details typedef struct { char symbol; int arg_count; // Number of arguments excluding the executable path. char **args; // Complete argument list for execv: executable + arguments + NULL terminator. } Player;
+#define PIPE(fd) socketpair(AF_UNIX, SOCK_STREAM, PF_UNIX, fd)
 
-int main() { 
-    int grid_width, grid_height, streak_size, player_count;
-    // Read grid dimensions, streak size, and number of players.
-    if (scanf("%d %d %d %d", &grid_width, &grid_height, &streak_size, &player_count) != 4) {
-        fprintf(stderr, "Error reading game configuration\n");
-        exit(EXIT_FAILURE);
+struct PlayerInfo {
+    char symbol;
+    std::vector<char*> argv;
+    pid_t pid;
+    int fd_read_write;   // server reads from this
+};
+
+int grid_width, grid_height, streak_size, player_count;
+std::vector<PlayerInfo> players;
+
+void read_input() {
+    std::cin >> grid_width >> grid_height >> streak_size >> player_count;
+
+    for (int i = 0; i < player_count; ++i) {
+        PlayerInfo player;
+        int arg_count;
+
+        std::cin >> player.symbol >> arg_count;
+
+        // Read the executable path and arguments
+        for (int j = 0; j <= arg_count; ++j) {
+            std::string arg;
+            std::cin >> arg;
+            //std::cout << "arg: " << arg << "\n";
+            player.argv.push_back(&arg[0]);
+        }
+        player.argv.push_back(nullptr);
+
+        players.push_back(player);
     }
+}
 
-    // Allocate array to hold configurations for all players.
-    /*Player *players = malloc(player_count * sizeof(Player));
-    if (players == NULL) {
-        perror("malloc");
-        exit(EXIT_FAILURE);
+int main() {
+    read_input();
+
+    std::cout << "Grid: " << grid_width << "x" << grid_height << "\n";
+    std::cout << "Streak size: " << streak_size << "\n";
+    std::cout << "Player count: " << player_count << "\n";
+
+    for (int i = 0; i < player_count; ++i) {
+        PlayerInfo& p = players[i];
+        std::cout << "Player " << i + 1 << ": " << p.symbol << "\n";
+        std::cout << "  Executable and args:";
+
+        // Print until we hit the nullptr
+        for (size_t j = 0; j < p.argv.size() - 1; ++j) {
+            std::cout << " " << p.argv[j];
+        }
+        std::cout << "\n";
     }
-
-    // For each player, read the symbol, argument count,
-    // and then the executable path with its arguments.
-    for (int i = 0; i < player_count; i++) {
-        char symbol;
-        int total_args;  // Number of additional arguments (excluding executable path)
-
-        // Read player symbol and the number of arguments.
-        if (scanf(" %c %d", &symbol, &total_args) != 2) {
-            fprintf(stderr, "Error reading player info\n");
-            exit(EXIT_FAILURE);
-        }
-        players[i].symbol = symbol;
-        players[i].arg_count = total_args;
-
-        // Allocate room for: executable path + total_args arguments + NULL terminator.
-        int token_count = total_args + 2;
-        players[i].args = malloc(token_count * sizeof(char *));
-        if (players[i].args == NULL) {
-            perror("malloc");
-            exit(EXIT_FAILURE);
-        }
-
-        // Read the executable path.
-        players[i].args[0] = malloc(256 * sizeof(char));
-        if (players[i].args[0] == NULL || scanf("%s", players[i].args[0]) != 1) {
-            fprintf(stderr, "Error reading executable path\n");
-            exit(EXIT_FAILURE);
-        }
-
-        // Read each additional argument.
-        for (int j = 1; j <= total_args; j++) {
-            players[i].args[j] = malloc(256 * sizeof(char));
-            if (players[i].args[j] == NULL || scanf("%s", players[i].args[j]) != 1) {
-                fprintf(stderr, "Error reading player argument\n");
-                exit(EXIT_FAILURE);
-            }
-        }
-        // Null-terminate the args array for execv().
-        players[i].args[token_count - 1] = NULL;
-    }
-
-    // At this point, for a player "X 4" with the next line:
-    // "./random_player X 100 5 5"
-    // the following will be stored:
-    // players[i].args[0] = "./random_player"
-    // players[i].args[1] = "X"
-    // players[i].args[2] = "100"
-    // players[i].args[3] = "5"
-    // players[i].args[4] = "5"
-    // players[i].args[5] = NULL
-
-    // [Further code to create pipes, fork, and handle game logic goes here]
-
-    // Free allocated memory (for production code, free all allocations when done).
-    for (int i = 0; i < player_count; i++){
-        int token_count = players[i].arg_count + 2;
-        for (int j = 0; j < token_count; j++){
-            free(players[i].args[j]);
-        }
-        free(players[i].args);
-    }
-    free(players);*/
 
     return 0;
 }
